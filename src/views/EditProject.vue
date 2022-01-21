3300e59737db3024c2619c7bb48e6cc381fb83d2
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-21 19:46:29 -0300

    feat: create EditProject component

---
+++ src/views/EditProject.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1>Edit project</h1>
+  <form @submit.prevent="handleSubmit">
+    <label>Title:</label>
+    <input type="text" v-model="title" required>
+    <label>Details:</label>
+    <textarea v-model="details" required></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: [ 'id' ],
+  data() {
+    return {
+      title: '',
+      details: '',
+      uri: `http://localhost:3000/projects/${this.id}`,
+    }
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then((data) => {
+        this.title = data.title
+        this.details = data.details
+      })
+      .catch((error) => console.error(error))
+  }
+}
+</script>
+
+<style>
+
+</style>
