dce3c32863d0b86057dda58a73aea2f383c283e2
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-21 14:48:12 -0300

    feat: add SingleProject component

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="project">
+    <div class="actions">
+      <h3 @click="toggleShowDetails">{{ project.title }}</h3>
+    </div>
+    <div v-if="showDetails">
+      <div class="details">
+        <p>{{ project.details }}</p>
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: [ 'project' ],	
+  data() {
+    return {
+      showDetails: false
+    }
+  },
+  methods: {
+    toggleShowDetails() {
+      this.showDetails = !this.showDetails
+    }
+  }
+}
+</script>
+
+<style>
+  .project {
+    margin: 20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
+    border-left: 4px solid #e90074;
+  }
+
+  h3 {
+    cursor: pointer;
+  }
+</style>
