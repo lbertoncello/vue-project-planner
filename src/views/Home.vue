9ae866f97d7d04badb8b074ef275c57514f29280
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-21 10:43:08 -0300

    feat(Home): add project informations on the page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <p>{{ project.title }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {
+  components: {},
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then((res) => res.json())
+      .then((data) => this.projects = data)
+      .catch((error) => console.error(error.message))
   }
 }
 </script>
