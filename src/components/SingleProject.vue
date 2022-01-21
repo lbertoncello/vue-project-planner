b8c4b7d645d57e3fbec662ec9d7d21265b7b90bf
lbertoncello <lucas_b_andrade@hotmail.com>
2022-01-21 15:06:57 -0300

    feat(SingleProject): add action icons

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -2,13 +2,17 @@
   <div class="project">
     <div class="actions">
       <h3 @click="toggleShowDetails">{{ project.title }}</h3>
+      <div class="icons">
+        <span class="material-icons">edit</span>
+        <span class="material-icons">delete</span>
+        <span class="material-icons">done</span>
+      </div>
     </div>
     <div v-if="showDetails">
       <div class="details">
         <p>{{ project.details }}</p>
       </div>
     </div>
-    
   </div>
 </template>
 
@@ -37,8 +41,21 @@ export default {
     box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
     border-left: 4px solid #e90074;
   }
-
   h3 {
     cursor: pointer;
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover {
+    color: #777;
   }
 </style>
